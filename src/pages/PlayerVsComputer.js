e6bf2a75b5292b09034b18b0e0756e6f3000ab91
Nitin Sharma <nitin.sharma649@gmail.com>
2021-03-24 02:21:14 +0530

    Added jQuery, Computer Decision Making Logic, Added Moves list on right

--- src/pages/PlayerVsComputer.js
+++ src/pages/PlayerVsComputer.js
@@ -1,37 +1,46 @@
 import {useState, useEffect} from 'react';
 import Chess from 'chess.js';
 import Chessboard from "../src/Chessboard";
+import MoveCalculation from "../models/MoveCalculation";
+const $ = window.$;
 
-function PVC({children}){
+function PVC({children, setGameContext, whenPieceMoved, onGameOver}){
     const [fen, setFen] = useState('start');
     const [squareStyles, setSquareStyles] = useState({});
     const [pieceSquare, setPieceSquare] = useState('');
-
-    let game = null;
+    const [game, setGame] = useState(null);
+    const [currentPosition, setCurrentPosition] = useState(()=>{});
 
     useEffect(()=>{
-        game = new Chess();
+        setGame(new Chess());
     }, []);
 
-    const makeRandomMove = () => {
-        let possibleMoves = game.moves();
+    const makeComputerMove = () => {
+        MoveCalculation(null, null, onGameOver).makeBestMove({...game, ugly_move: function(move_obj, options) {
+                var pretty_move = game.make_pretty(move_obj);
+                game.make_move(move_obj);
 
-        // exit if the game is over
-        if (
-            game.game_over() === true ||
-            game.in_draw() === true ||
-            possibleMoves.length === 0
-        )
-            return;
-
-        let randomIndex = Math.floor(Math.random() * possibleMoves.length);
-        game.move(possibleMoves[randomIndex]);
-        setFen(game.fen());
-        setSquareStyles({
-            [game.history({ verbose: true })[game.history().length - 1].to]: {
-                backgroundColor: 'DarkTurquoise'
-            }
-        })
+                return pretty_move;
+            }}, currentPosition);
+        // let possibleMoves = game.moves();
+        //
+        // // exit if the game is over
+        // if (
+        //     game.game_over() === true ||
+        //     game.in_draw() === true ||
+        //     possibleMoves.length === 0
+        // )
+        //     return;
+        //
+        // let randomIndex = Math.floor(Math.random() * possibleMoves.length);
+        // game.move(possibleMoves[randomIndex]);
+        // setFen(game.fen());
+        // setSquareStyles({
+        //     [game.history({ verbose: true })[game.history().length - 1].to]: {
+        //         backgroundColor: 'DarkTurquoise'
+        //     }
+        // })
+        whenPieceMoved(game.history({verbose: true}));
     };
 
     const onDrop = ({ sourceSquare, targetSquare }) => {
@@ -46,7 +55,8 @@ function PVC({children}){
         if (move === null) return;
 
         setFen(game.fen());
-        window.setTimeout(makeRandomMove, 1000);
+        whenPieceMoved(game.history({verbose: true}));
+        window.setTimeout(makeComputerMove, 1000);
     };
 
     const onSquareClick = square => {
@@ -63,31 +73,49 @@ function PVC({children}){
         if (move === null) return;
 
         setFen(game.fen());
-        window.setTimeout(makeRandomMove, 1000);
+        whenPieceMoved(game.history({verbose: true}));
+        window.setTimeout(makeComputerMove, 1000);
     };
 
-    return children({ position: fen, onDrop, onSquareClick, squareStyles });
+    return (
+        <Chessboard
+            calcWidth={({ screenWidth }) => (screenWidth < 500 ? 350 : 480)}
+            id="humanVsComputer"
+            position={fen}
+            getPosition={setCurrentPosition}
+            onDrop={(e)=>onDrop(e) && console.log(e) }
+            boardStyle={{
+                borderRadius: '5px',
+                boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+            }}
+            onSquareClick={onSquareClick}
+            squareStyles={squareStyles}
+        />
+    );
 }
 
-export default function PlayerVsComputer() {
+export default function PlayerVsComputer({user= 'Avinash'}) {
+    const [movesList, setMovesList] = useState([]);
+    const [gameContext, setGameContext] = useState(null);
+    const [currentPosition, setCurrentPosition] = useState(null);
     return (
+        <div style={{display: 'flex'}}>
         <div style={{ backgroundColor: 'white', padding: 20, borderRadius: 10}}>
-            <PVC>
-                {({ position, onDrop, onSquareClick, squareStyles }) => (
-                    <Chessboard
-                        calcWidth={({ screenWidth }) => (screenWidth < 500 ? 350 : 480)}
-                        id="humanVsRandom"
-                        position={position}
-                        onDrop={onDrop}
-                        boardStyle={{
-                            borderRadius: '5px',
-                            boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
-                        }}
-                        onSquareClick={onSquareClick}
-                        squareStyles={squareStyles}
-                    />
-                )}
+            <h5 style={{color: "green"}}>{user} <span style={{color: 'black'}}>V/s</span> <span style={{color: 'red'}}>Computer</span></h5>
+            <PVC whenPieceMoved={setMovesList} computer={MoveCalculation}>
+
             </PVC>
+        </div>
+        <div style={{ marginLeft: 10, backgroundColor: 'white', padding: 20, borderRadius: 10}}>
+            <h4>Moves List</h4>
+            <hr/>
+            <ul>
+                {movesList.map(m=>(
+                    <li>{m.color=="w"?user:"Computer"}: {m.from} -> {m.to}</li>
+                ))}
+            {/*{movesList?.map(m=><li>{m}</li>)}*/}
+            </ul>
+        </div>
         </div>
     );
 }
